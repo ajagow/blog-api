e11bac9d41a38d589b04e0f2c087145077ae0321
ajagow <budgiejag@gmail.com>
2019-07-11 10:17:24 -0400

    added gunicorn

--- run.py
+++ run.py
@@ -1,10 +1,22 @@
-# /run.py
-import os
+# # /run.py
+# import os
 
-from src.app import create_app
+# from src.app import create_app
 
-if __name__ == '__main__':
-  env_name = os.getenv('FLASK_ENV')
-  app = create_app(env_name)
-  # run app
-  app.run()
+# if __name__ == '__main__':
+#   env_name = os.getenv('FLASK_ENV')
+#   app = create_app(env_name)
+#   # run app
+#   app.run()
+
+ # /run.py
+ import os
+ from src.app import create_app
+
+ env_name = os.getenv('FLASK_ENV')
+ app = create_app(env_name)
+
+ if __name__ == '__main__':
+   port = os.getenv('PORT')
+   # run app
+   app.run(host='0.0.0.0', port=port)
